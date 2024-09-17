f84ce099ae103d042ec5c4105adb5daefeec5ac3
cornellpepperdayblake9 <cornellpepperdayblake9a@hotmail.com>
2024-09-17 03:57:00 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("f7bc71ebad6eb25372f86f5a1700524409f825c73e39ca9d0ec791acc9bf7e81", "f7bc71ebad6eb25372f86f5a1700524409f825c73e39ca9d0ec791acc9bf7e81") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
